70eb805c1fb9163d33971ea064a837f0f8a6da19
Girish Nandan <jngnandan@gmail.com>
2022-10-11 22:03:05 +0100

    page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ContactUs from './views/ContactUs';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+import Boxing from './views/Boxing'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen mb-auto'>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Boxing />} />
+          <Route path='/contactus' element={<ContactUs />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+
+import menu from '../assets/icons/menu.svg'
+import close from '../assets/icons/close.svg'
+
+
+
+export default function Header() {
+  const [navbarOpen, setNavbarOpen] = useState(true);
+
+  return (
+    <div>
+        <div className='shadow flex flex-row justify-between items-center h-16 px-6  lg:px-44 xl:px-60 font-medium text-sm'>
+          <div className='' onClick={() => setNavbarOpen(true)}>
+          <Link to='/'>
+            <h1 className='w-30'>Knockout Zone</h1>
+          </Link>
+          </div>
+
+          <div className='flex flex-row m-5 invisible  md:visible'>
+            <Link to='/contactus'>
+              <p className='mx-2'>Contact us</p>
+            </Link>
+          </div>
+
+          {navbarOpen?
+            <button onClick={() => setNavbarOpen(!navbarOpen)} className='flex flex-col justify-center items-center sm:block md:hidden'>
+            <img src={menu} className='h-5' />
+          </button>
+          :
+          <button onClick={() => setNavbarOpen(!navbarOpen)} className='flex flex-col justify-center items-center sm:block md:hidden'>
+            <img src={close} className='h-5' />
+          </button>
+          }
+        </div>
+
+        {!navbarOpen &&
+        <div className='px-6 font-medium text-sm pb-4 bg-red-700'>
+          <Link to='/security-services' onClick={() => setNavbarOpen(!navbarOpen)}>
+                <p className='py-3 text-white hover:text-black w-full'>Security Services</p>
+            </Link>
+            <Link to='/clearning-services' onClick={() => setNavbarOpen(!navbarOpen)}>
+                <p className='py-3 text-white hover:text-black w-full'>Cleaning Services</p>
+            </Link>
+            <Link to='/boxing' onClick={() => setNavbarOpen(!navbarOpen)}>
+                <p className='py-3 text-white hover:text-black w-full'>Boxing</p>
+            </Link>
+            <Link to='/contactus' onClick={() => setNavbarOpen(!navbarOpen)}>
+              <p className='py-3 text-white hover:text-black w-full'>Contact us</p>
+            </Link>
+        </div>
+        }
+      </div>
+  )
+}
